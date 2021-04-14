bd9f570a4cee30bdf055e4ddb0f946d68d1b7724
Vadim <shadrin686@gmail.com>
2021-04-14 09:30:59 +0600

    second version

--- lesson/scripts/script.js
+++ lesson/scripts/script.js
@@ -1,35 +1,20 @@
-"use strict";
-// // function factorial(n) {
-// //  return (n != 1) ? n * factorial (n - 1) : 1;
-// // }
-// function factorial(n) {
-//     if (n == 0) {
-//         return 1;
-//     } else {
-//         return factorial(n - 1) * n;
-//     }
-// }
-// console.log(factorial(1));
-// console.log(factorial(2));
-// console.log(factorial(3));
-// console.log(factorial(4));
-// console.log(factorial(5));
-// console.log(factorial(6));
-// console.log(factorial(7));
-// let a = 2 ** 4;
-// console.log(a);
-// let c;
-// console.log(typeof c);
-function showPrimes(n) {
-  nextPrime: for (let i = 2; i < n; i++) {
+const numberOfFilms = +prompt("How much movies do you watch&", '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
 
-    for (let j = 2; j < i; j++) {
-      if (i % j == 0) continue nextPrime;
-    }
 
-    console.log(i); // простое
-  }
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?');
+          personalMovieDB.movies[a] = b;
 }
-console.log(showPrimes(31));
 
 
+
+
+console.log(personalMovieDB);
